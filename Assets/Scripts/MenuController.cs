a8d7a89b96fcecd8a13a87e2cb26cc0d17198c2c
asmaalriyamia <33996600+asmaalriyamia@users.noreply.github.com>
2019-04-23 22:28:37 +0400

    merged

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -7,54 +7,98 @@ public class MenuController : MonoBehaviour
 {
 
     public Button playButton;
+
+    public Image Msgbox;
+    public Text ShowMsgErrorText;
+    public Button CancelBtn;
     public Toggle IsMale;
     public Toggle IsFemale;
+    public Image PlayerImg;
 
-    public Text ShowMsgError;
+   
+
     // Start is called before the first frame update
     void Start()
     {
-        playButton.onClick.AddListener(ToGameScene);
+        HideObj();
 
+        playButton.onClick.AddListener(ToGameScene);
+        CancelBtn.onClick.AddListener(HideObj);
     }
 
     // Update is called once per frame
     void Update()
     {
+     
 
     }
-
 
     void ToGameScene()
     {
-
+        
         if (IsMale.isOn)
         {
+    
             MoveToScenea();
-        }
-        else if (IsFemale.isOn)
+            PlayerImg.color = Color.blue;
 
+        }
+        else if (IsFemale.isOn )
         {
+          
             MoveToScenea();
+            PlayerImg.color = Color.red;
+            
+
         }
         else
         {
-            ShowMsgError.text = "Plase select Gender";
+            ShowObjError();
         }
     }
      
+  
+    
 
-         
+    void HideObj()
+    {
+        Msgbox.enabled = false;
+        ShowMsgErrorText.enabled = false;
+        CancelBtn.gameObject.SetActive(false);
+        PlayerImg.enabled = false;
+        ShowMsgErrorText.text = "";
+    }
+
+
+    void ShowObjError()
+    {
+        Msgbox.enabled = true;
+        ShowMsgErrorText.enabled = true;
+        CancelBtn.gameObject.SetActive(true);
+        ShowMsgErrorText.text = "Plase select Gender";
+    }
+
+
+    void ShowObjCorrect()
+    {
+        Msgbox.enabled = true;
+        PlayerImg.enabled = true;
+    }
+
+
+
     void MoveToScenea()
     {
-        SceneManager.LoadScene("GameScene");
-        ShowMsgError.text = "";
+        for (int TimerMovScen = 0; TimerMovScen <=100; TimerMovScen++)
+        {
+            ShowObjCorrect();
+            SceneManager.LoadScene("GameScene");
+        }
     }
 
 
     public void SoundOn()
     {
-
         AudioScript.Instance.ToggleMuteSound();
     }
 
