624077c3538fbb9006a2b95ed7b7a8c49394ba18
sawsanalraisi <sawsanalraisi00@hotmail.com>
2019-04-28 00:46:18 +0400

    Update5

---
+++ Nashaba-Develop/Nashaba/Assets/Scripts/LoadingProgressBar.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingProgressBar : MonoBehaviour
+{
+    public Transform LoadingBar;
+    public Transform TextIndicator;
+
+    [SerializeField]
+    private float currentAmount;
+    [SerializeField]
+    private float Speed=10f;
+
+
+
+
+    void Update()
+    {
+        if (currentAmount < 100)
+        {
+            currentAmount += Speed * Time.deltaTime;
+            TextIndicator.GetComponent<Text>().text = ((int)currentAmount).ToString() + "%";
+        }
+        else
+        {
+
+            TextIndicator.GetComponent<Text>().text = "Done!";
+
+           for(int i=0;i<5;i++)
+            {
+                SceneManager.LoadScene("Menu");
+            }
+
+        }
+
+        LoadingBar.GetComponent<Image>().fillAmount = currentAmount / 100;
+
+    }
+
+}
